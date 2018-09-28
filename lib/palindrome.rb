f728a66067fe91a2f3435d50a9cc787b9bd1552e
JNelson <nelson.jimmy@gmail.com>
2018-09-28 06:55:52 -0600

    Finishes palindrome method.

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,19 +1,22 @@
 class Palindrome
   def longest_palindrome(string)
-    palindrome = []
+    all_palindromes = []
+
     letters = string.chars
-    first = letters.first
-    last = letters.last
-    if check_first_and_last(first, last)
-
-  end
-
-  def check_first_and_last(first, last)
-    if first == last
-      true
-    else
-      false
+    letters.each_with_index do |letter, index|
+      repetitions = letters.length - index
+      comparison = letters.length - 1
+      repetitions.times do
+        forwards = letters[index..comparison]
+        backwards = forwards.reverse
+        if forwards == backwards
+          all_palindromes << forwards
+        end
+        comparison -= 1
+      end
     end
+    longest = all_palindromes.max_by { |palindrome| palindrome.length }
+    longest.join
   end
 
 end
