c47bd31441fd7964589757c4e6e3a6c8360b8a71
JNelson <nelson.jimmy@gmail.com>
2018-10-09 12:19:19 -0600

    Tweaks conditionals to determine where the datasets are filtering through.

--- lib/bribe.rb
+++ lib/bribe.rb
@@ -1,22 +1,27 @@
 class Bribe
   def minimumBribes(q)
     bribes = 0
+    bribes_obverse = 0
     q.each_with_index do |value, index|
       offset_index = index + 1
       difference = value - offset_index
-      if difference > 0 && value < (offset_index + 3)
+      if difference > 0 && value < (offset_index + 5)
         bribes += difference
-      elsif difference > 0 && value > (offset_index + 2)
-        puts "Too chaotic"
+      elsif value > (offset_index + 5) || value < (offset_index - 5)
+        binding.pry
+        # puts "Too chaotic"
         return "Too chaotic"
       elsif difference < -3
         extra = -3 - difference
         bribes += extra
+      elsif difference < 0
+        bribes_obverse += difference
       else
-        bribes += 0
+        puts value
       end
     end
-    puts bribes
-    bribes
+    # puts bribes
+    puts "It took #{bribes} bribes"
+    puts "The bribes obverse is #{bribes_obverse}"
   end
 end
