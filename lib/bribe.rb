0d046210785124d0e313909fd9649cbadbb26862
JNelson <nelson.jimmy@gmail.com>
2018-10-09 13:12:53 -0600

    Updates conditionals to allow for adding more points for larger differences.

--- lib/bribe.rb
+++ lib/bribe.rb
@@ -1,27 +1,23 @@
 class Bribe
   def minimumBribes(q)
     bribes = 0
-    bribes_obverse = 0
     q.each_with_index do |value, index|
       offset_index = index + 1
       difference = value - offset_index
-      if difference > 0 && value < (offset_index + 5)
-        bribes += difference
-      elsif value > (offset_index + 5) || value < (offset_index - 5)
-        binding.pry
-        # puts "Too chaotic"
+      if difference > 0 && value > offset_index + 2 
+        puts "Too chaotic"
         return "Too chaotic"
-      elsif difference < -3
-        extra = -3 - difference
-        bribes += extra
-      elsif difference < 0
-        bribes_obverse += difference
+      elsif difference > 0
+        bribes += difference
+      elsif difference == -4
+        bribes += difference.abs
+      elsif difference == -5
+        bribes += (difference.abs + 2)
       else
-        puts value
+        bribes += 0
       end
     end
-    # puts bribes
-    puts "It took #{bribes} bribes"
-    puts "The bribes obverse is #{bribes_obverse}"
+    puts bribes
+    bribes
   end
 end
