cecb8ab592c87587f0ddc8a5ed2fe3fea707331f
JNelson <nelson.jimmy@gmail.com>
2018-10-17 10:41:56 -0600

    Refactors longest string of unique characters method to combine the strings before turning them into an array.

--- lib/longest_string.rb
+++ lib/longest_string.rb
@@ -1,9 +1,5 @@
 class LongestString
   def longest(a1, a2)
-    a_array = a1.chars
-    b_array = a2.chars
-    combined_array = [a_array, b_array]
-    flattened = combined_array.flatten
-    flattened.uniq.sort.join
+    (a1 + a2).chars.uniq.sort.join
   end
 end
