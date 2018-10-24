2b7f6ebec61ff799b2cfc75a039ba638710f1223
JNelson <nelson.jimmy@gmail.com>
2018-10-24 14:00:56 -0600

    Updates tests for bouncing ball to account for edge cases.

--- test/bouncing_ball_test.rb
+++ test/bouncing_ball_test.rb
@@ -11,15 +11,36 @@ class BouncingBallTest < Minitest::Test
 
   def test_it_works_twice
     bounces = BouncingBall.new
-    expected = 3
-    actual = bounces.bouncingBall(3, 0.66, 1.5)
+    expected = 15
+    actual = bounces.bouncingBall(30, 0.66, 1.5)
     assert_equal expected, actual
   end
 
   def test_it_works_three_times
     bounces = BouncingBall.new
+    expected = 21
+    actual = bounces.bouncingBall(30, 0.75, 1.5)
+    assert_equal expected, actual
+  end
+
+  def test_it_works_four_times
+    bounces = BouncingBall.new
     expected = 3
-    actual = bounces.bouncingBall(3, 0.66, 1.5)
+    actual = bounces.bouncingBall(30, 0.4, 10)
+    assert_equal expected, actual
+  end
+
+  def test_it_gives_negative_one_once
+    bounces = BouncingBall.new
+    expected = -1
+    actual = bounces.bouncingBall(40, 1, 10)
+    assert_equal expected, actual
+  end
+
+  def test_it_gives_negative_one_twice
+    bounces = BouncingBall.new
+    expected = -1
+    actual = bounces.bouncingBall(-5, 0.66, 1.5)
     assert_equal expected, actual
   end
 end
