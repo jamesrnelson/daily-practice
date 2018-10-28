d76cd1cf5f238a6e02d6360f1605a41850540c86
JNelson <nelson.jimmy@gmail.com>
2018-10-28 09:07:02 -0600

    Refactored gap method into multiple discreet methods.

--- lib/primes.rb
+++ lib/primes.rb
@@ -3,24 +3,36 @@ class Primes
     if m > n || m < 2 || g < 2
       nil
     else
-      initial_set = (2..n).to_a
-      primes = initial_set.each do |num|
-        multiplier = 2
-        while (num * multiplier) < n
-          initial_set.delete(num * multiplier)
-          multiplier += 1
-        end
+      crunch_the_numbers(g, m, n)
+    end
+  end
+
+  def crunch_the_numbers(gap, start, last)
+    initial_set = (2..last).to_a
+    primes = find_primes(initial_set, last)
+    final = primes.reject { |prime| prime < start }
+    matching_values(final, gap)
+  end
+
+  def find_primes(initial_set, last)
+    initial_set.each do |num|
+      multiplier = 2
+      while (num * multiplier) < last
+        initial_set.delete(num * multiplier)
+        multiplier += 1
       end
-      final_set = primes.reject { |prime| prime < m }
-      if final_set.length < 2
-        nil
-      else
-        index = 1
-        while index < final_set.length
-          return [final_set[index - 1], final_set[index]] if final_set[index] - final_set[index - 1] == g
-          index += 1
-        end
-      end
+    end
+  end
+
+  def matching_values(final, gap)
+    final.length < 2 ? nil : compare_differences(final, gap)
+  end
+
+  def compare_differences(final, gap)
+    i = 1
+    while i < final.length
+      return [final[i - 1], final[i]] if final[i] - final[i - 1] == gap
+      i += 1
     end
   end
 end
