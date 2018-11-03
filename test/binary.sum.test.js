0902573e3a33be204091d342eb7268ca13131a08
JNelson <nelson.jimmy@gmail.com>
2018-11-03 11:46:05 -0600

    Updates test to have better ES6 syntax.

--- test/binary.sum.test.js
+++ test/binary.sum.test.js
@@ -1,16 +1,16 @@
-import { expect } from "chai"
-import BinarySum from "../lib/binarySum"
+import { expect } from "chai";
+import BinarySum from "../lib/binarySum";
 
 describe("BinarySum", () => {
   describe("functions", () => {
-    const binary = new BinarySum;
+    const binary = new BinarySum();
 
     it("should work as expected", () => {
       let num1 = 5;
       let num2 = 3;
       let actual = binary.addAndConvertToBinary(num1, num2);
-      let expected = 1000
-      expect(actual).to.eql(expected)
-    })
-  })
-})
+      let expected = 1000;
+      expect(actual).to.eql(expected);
+    });
+  });
+});
